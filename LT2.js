0086dc4c7a28e39b53e3d870c68bcf51d702726e
ARCHIT <127728488+Archit0308@users.noreply.github.com>
2023-03-26 22:51:59 +0530

    Update LT2.js

--- LT2.js
+++ LT2.js
@@ -1,70 +1,72 @@
-const fromText = document.querySelector(".from-text"),
-toText = document.querySelector(".to-text"),
-exchageIcon = document.querySelector(".exchange"),
-selectTag = document.querySelectorAll("select"),
-icons = document.querySelectorAll(".row i");
-translateBtn = document.querySelector("button"),
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTags = document.querySelectorAll("select");
+const icons = document.querySelectorAll(".row i");
+const translateBtn = document.querySelector("button");
 
-selectTag.forEach((tag, id) => {
-    for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
-        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
-        tag.insertAdjacentHTML("beforeend", option);
-    }
+selectTags.forEach((tag, id) => {
+  for (const countryCode in countries) {
+    const selected =
+      id === 0
+        ? countryCode === "en-GB"
+          ? "selected"
+          : ""
+        : countryCode === "hi-IN"
+        ? "selected"
+        : "";
+    const option = `<option ${selected} value="${countryCode}">${countries[countryCode]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
+  }
 });
 
-exchageIcon.addEventListener("click", () => {
-    let tempText = fromText.value,
-    tempLang = selectTag[0].value;
-    fromText.value = toText.value;
-    toText.value = tempText;
-    selectTag[0].value = selectTag[1].value;
-    selectTag[1].value = tempLang;
+exchangeIcon.addEventListener("click", () => {
+  const tempText = fromText.value;
+  const tempLang = selectTags[0].value;
+  fromText.value = toText.value;
+  toText.value = tempText;
+  selectTags[0].value = selectTags[1].value;
+  selectTags[1].value = tempLang;
 });
 
-fromText.addEventListener("keyup", () => {
-    if(!fromText.value) {
-        toText.value = "";
-    }
+fromText.addEventListener("input", () => {
+  if (!fromText.value.trim()) {
+    toText.value = "";
+  }
 });
 
 translateBtn.addEventListener("click", () => {
-    let text = fromText.value.trim(),
-    translateFrom = selectTag[0].value,
-    translateTo = selectTag[1].value;
-    if(!text) return;
-    toText.setAttribute("placeholder", "Translating...");
-    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
-    fetch(apiUrl).then(res => res.json()).then(data => {
-        toText.value = data.responseData.translatedText;
-        data.matches.forEach(data => {
-            if(data.id === 0) {
-                toText.value = data.translation;
-            }
-        });
-        toText.setAttribute("placeholder", "Translation");
+  const text = fromText.value.trim();
+  const translateFrom = selectTags[0].value;
+  const translateTo = selectTags[1].value;
+  if (!text) return;
+  toText.setAttribute("placeholder", "Translating...");
+  const apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      const translation = data.responseData.translatedText;
+      toText.value = translation;
+      data.matches.forEach((match) => {
+        if (match.id === 0) {
+          toText.value = match.translation;
+        }
+      });
+      toText.setAttribute("placeholder", "Translation");
     });
 });
 
-icons.forEach(icon => {
-    icon.addEventListener("click", ({target}) => {
-        if(!fromText.value || !toText.value) return;
-        if(target.classList.contains("fa-copy")) {
-            if(target.id == "from") {
-                navigator.clipboard.writeText(fromText.value);
-            } else {
-                navigator.clipboard.writeText(toText.value);
-            }
-        } else {
-            let utterance;
-            if(target.id == "from") {
-                utterance = new SpeechSynthesisUtterance(fromText.value);
-                utterance.lang = selectTag[0].value;
-            } else {
-                utterance = new SpeechSynthesisUtterance(toText.value);
-                utterance.lang = selectTag[1].value;
-            }
-            speechSynthesis.speak(utterance);
-        }
-    });
+icons.forEach((icon) => {
+  icon.addEventListener("click", ({ target }) => {
+    if (!fromText.value.trim() || !toText.value.trim()) return;
+    if (target.classList.contains("fa-copy")) {
+      const text = target.id === "from" ? fromText.value : toText.value;
+      navigator.clipboard.writeText(text);
+    } else {
+      const text = target.id === "from" ? fromText.value : toText.value;
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = target.id === "from" ? selectTags[0].value : selectTags[1].value;
+      speechSynthesis.speak(utterance);
+    }
+  });
 });
